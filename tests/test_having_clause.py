02f0aa894b04b1973dc6e3c531084e4b974f6340
Flynn Wang <wang.flynn@gmail.com>
2013-08-05 21:38:51 +0800

    add test for using correct table in boolean primary

--- tests/test_having_clause.py
+++ tests/test_having_clause.py
@@ -11,3 +11,16 @@ class HavingColumnConditionTest(SelectTestBase):
         return [[id_, len(list(g))]
                 for id_, g in groupby(self.rows, lambda x: x[0])
                 if id_ < 5]
+
+
+class HavingColumnConditionTest2(SelectTestBase):
+    sql = "select id, name, count(id) from user groupby id having name < 'c'"
+
+    def expected_select_result(self):
+        rows = []
+        for id_, g in groupby(self.rows, lambda x: x[0]):
+            g = list(g)
+            name = g[0][1]
+            if name < 'c':
+                rows.append([id_, name, len(g)])
+        return rows
