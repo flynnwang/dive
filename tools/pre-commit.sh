6b41946d7c98bc25335b4e35f1f95fa6b7d77fe0
Flynn Wang <wang.flynn@gmail.com>
2013-08-12 21:08:50 +0800

    add pre-commit hook

---
+++ tools/pre-commit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# cd dive
+# ln -s ../../tools/pre-commit.sh .git/hooks/pre-commit
+
+git stash -q --keep-index
+
+echo 'run tests...'
+fab test:options=xs
+RESULT=$?
+
+git stash pop -q
+
+[ $RESULT -ne 0 ] && exit 1
+exit 0
