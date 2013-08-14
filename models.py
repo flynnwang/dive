fa4659bfc6f953fabac9f16f82895ca75b7a8824
Flynn Wang <wang.flynn@gmail.com>
2013-08-14 12:05:56 +0800

    add float, time, datetime support

--- models.py
+++ models.py
@@ -19,12 +19,36 @@ class Integer(Model):
 
     def cast(self, s):
         return int(s)
+
+
+class Float(Model):
+
+    def cast(self, s):
+        return float(s)
         
 
-class Date(object):
+class Date(Model):
 
     def __init__(self, format="%Y/%m/%d"):
         self.format = format
 
     def cast(self, s):
         return datetime.strptime(s, self.format).date()
+
+
+class Time(Model):
+
+    def __init__(self, format="%H:%M:%S"):
+        self.format = format
+
+    def cast(self, s):
+        return datetime.strptime(s, self.format).time()
+
+
+class Datetime(Model):
+
+    def __init__(self, format="%Y/%m/%d %H:%M:%S"):
+        self.format = format
+
+    def cast(self, s):
+        return datetime.strptime(s, self.format)
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,8 +2,8 @@
 
 import unittest
 
-from datetime import date
-from dive.models import Date
+from datetime import date, time, datetime
+from dive.models import Date, Time, Datetime
 
 
 class DateTest(unittest.TestCase):
@@ -19,3 +19,21 @@ class DateTest(unittest.TestCase):
         date_ = Date("%Y-%m-%d")
 
         assert date(2013, 8, 14) == date_.cast(date_str)
+
+
+class TimeTest(unittest.TestCase):
+
+    def test_date_cast(self):
+        time_str = "12:02:05"
+        time_ = Time()
+
+        assert time(12, 2, 5) == time_.cast(time_str)
+
+
+class DatetimeTest(unittest.TestCase):
+
+    def test_datetime_cast(self):
+        dt_str = "2013/8/14 12:02:05"
+        dt_ = Datetime()
+
+        assert datetime(2013, 8, 14, 12, 2, 5) == dt_.cast(dt_str)
